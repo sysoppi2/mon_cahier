f36e481751c76b6fa3f84f328a486c8f72849d38
sysop@pi2 <gallicejl74@gmail.com>
2019-09-02 18:40:49 +0200

    Mon app Django, premier commit

--- e_cahier/settings.py
+++ e_cahier/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'e_cahier',
 	'USER' : 'sysop',
 	'PASSWORD' : 'posys',
-	'HOST' : 'localhost',
+	'HOST' : '127.0.0.1',
 	'PORT' : '', # server port
 	'OPTIONS' : {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
     }
