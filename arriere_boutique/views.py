12740fb72d9f6d1f60871a85e1a8b6ec960edfc8
sysop@pi2 <gallicejl74@gmail.com>
2019-09-06 17:20:35 +0200

    modif html blog

--- arriere_boutique/views.py
+++ arriere_boutique/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here:
 
+def post_list(request):
+	return render(request, 'arriere_boutique/post_list.html', {})
--- e_cahier/urls.py
+++ e_cahier/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
@@ -20,6 +21,6 @@ from django.urls import include, path
 #from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-#	path('arriere_boutique/', include('arriere_boutique.urls')),
+	path('', include('arriere_boutique.urls')),
 	path('admin/', admin.site.urls),
 ]
